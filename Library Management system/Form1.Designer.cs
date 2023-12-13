645018504a57168f67720ff4fc93225fe5c945a9
Hakam Mohamed Fazeel <18mohamedfazeelh@roundwoodpark.co.uk>
2023-12-13 11:52:32 +0100

    hghjgh

--- Library Management system/Form1.Designer.cs
+++ Library Management system/Form1.Designer.cs
@@ -32,53 +32,72 @@
             LBbookid = new Label();
             LBoutput = new Label();
             BTNload = new Button();
+            DTgrid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)DTgrid).BeginInit();
             SuspendLayout();
             // 
             // TXBinput
             // 
-            TXBinput.Location = new Point(32, 63);
+            TXBinput.Location = new Point(40, 79);
+            TXBinput.Margin = new Padding(4);
             TXBinput.Name = "TXBinput";
-            TXBinput.Size = new Size(125, 27);
+            TXBinput.Size = new Size(155, 31);
             TXBinput.TabIndex = 0;
             // 
             // LBbookid
             // 
             LBbookid.AutoSize = true;
-            LBbookid.Location = new Point(31, 19);
+            LBbookid.Location = new Point(39, 24);
+            LBbookid.Margin = new Padding(4, 0, 4, 0);
             LBbookid.Name = "LBbookid";
-            LBbookid.Size = new Size(100, 20);
+            LBbookid.Size = new Size(123, 25);
             LBbookid.TabIndex = 1;
             LBbookid.Text = "Input Book ID";
             // 
             // LBoutput
             // 
             LBoutput.AutoSize = true;
-            LBoutput.Location = new Point(37, 117);
+            LBoutput.Location = new Point(46, 146);
+            LBoutput.Margin = new Padding(4, 0, 4, 0);
             LBoutput.Name = "LBoutput";
-            LBoutput.Size = new Size(0, 20);
+            LBoutput.Size = new Size(0, 25);
             LBoutput.TabIndex = 2;
             // 
             // BTNload
             // 
-            BTNload.Location = new Point(39, 158);
+            BTNload.Location = new Point(49, 198);
+            BTNload.Margin = new Padding(4);
             BTNload.Name = "BTNload";
-            BTNload.Size = new Size(94, 29);
+            BTNload.Size = new Size(118, 36);
             BTNload.TabIndex = 3;
             BTNload.Text = "Load";
             BTNload.UseVisualStyleBackColor = true;
             BTNload.Click += BTNload_Click;
             // 
+            // DTgrid
+            // 
+            DTgrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DTgrid.Location = new Point(252, 128);
+            DTgrid.Name = "DTgrid";
+            DTgrid.RowHeadersWidth = 62;
+            DTgrid.Size = new Size(360, 225);
+            DTgrid.TabIndex = 4;
+            // 
             // Window
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1000, 562);
+            Controls.Add(DTgrid);
             Controls.Add(BTNload);
             Controls.Add(LBoutput);
             Controls.Add(LBbookid);
             Controls.Add(TXBinput);
+            Margin = new Padding(4);
             Name = "Window";
             Text = "Library Management system";
+            Load += Window_Load;
+            ((System.ComponentModel.ISupportInitialize)DTgrid).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -89,5 +108,6 @@
         private Label LBbookid;
         private Label LBoutput;
         private Button BTNload;
+        private DataGridView DTgrid;
     }
 }
--- Library Management system/Form1.cs
+++ Library Management system/Form1.cs
@@ -1,7 +1,12 @@
+using System.Data;
+
 namespace Library_Management_system
 {
     public partial class Window : Form
     {
+
+        DataTable table;
+
         public Window()
         {
             InitializeComponent();
@@ -9,8 +14,16 @@ namespace Library_Management_system
 
         private void BTNload_Click(object sender, EventArgs e)
         {
-            string txt = TXBinput.Text;
-            LBoutput.Text = txt;
+            table.Rows.Add(TXBinput.Text, TXBinput.Text);
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("ID", typeof(String));
+            table.Columns.Add("Book name", typeof(String));
+
+            DTgrid.DataSource = table;
         }
     }
 }
