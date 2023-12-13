51f5f0ae5503bbc6fe07cf08e3f040e65609ddb6
Hakam Mohamed Fazeel <18mohamedfazeelh@roundwoodpark.co.uk>
2023-12-13 11:05:58 +0100

    hjsbfsd

--- Library Management system/Form1.cs
+++ Library Management system/Form1.cs
@@ -1,8 +1,8 @@
 namespace Library_Management_system
 {
-    public partial class Form1 : Form
+    public partial class Window : Form
     {
-        public Form1()
+        public Window()
         {
             InitializeComponent();
         }
